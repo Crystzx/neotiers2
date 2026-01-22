b3d1ef4869f2bb0341c9d7e052194cca2cb9ace4
Crystzx <crys1zxc@gmail.com>
2026-01-22 19:05:07 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -362,11 +362,11 @@ const highTierResults = [
   { name: "Crystzx", avatar: "https://render.crafty.gg/3d/bust/Crystzx", mode: "SpearSMP", modeIcon: "https://i.ibb.co/PsRW7n1L/e-1.png", tier: "HT3", region: "EU" },
 ];
 const liveTestResults = [
+  { name: "Syst3m4k", avatar: "https://render.crafty.gg/3d/bust/Syst3m4k", mode: "SpearSMP", modeIcon: "https://i.ibb.co/PsRW7n1L/e-1.png", tier: "HT5", region: "EU" },
   { name: "Semin2210", avatar: "https://render.crafty.gg/3d/bust/Semin2210", mode: "SpearSMP", modeIcon: "https://i.ibb.co/PsRW7n1L/e-1.png", tier: "LT3", region: "EU" },
   { name: "Fesh4k", avatar: "https://render.crafty.gg/3d/bust/Fesh4k", mode: "SpearSMP", modeIcon: "https://i.ibb.co/PsRW7n1L/e-1.png", tier: "LT3", region: "EU" },
   { name: "Tectechtechy", avatar: "https://render.crafty.gg/3d/bust/Tectechtechy", mode: "SpearSMP", modeIcon: "https://i.ibb.co/PsRW7n1L/e-1.png", tier: "HT4", region: "EU" },
   { name: "Yk_Mega", avatar: "https://render.crafty.gg/3d/bust/Yk_Mega", mode: "SpearSMP", modeIcon: "https://i.ibb.co/PsRW7n1L/e-1.png", tier: "LT4", region: "EU" },
-  { name: "Fr3ggyDemon", avatar: "https://render.crafty.gg/3d/bust/Fr3ggyDemon", mode: "SpearSMP", modeIcon: "https://i.ibb.co/PsRW7n1L/e-1.png", tier: "LT4", region: "EU" },
 ];
 
 /* https://pvptiers.com/icons/modes/crystal.webp - Vanilla */
